43ede4fc96e0f7f1111d432de4a24459f145c086
Valentin-Gruenewald <112621829+Valentin-Gruenewald@users.noreply.github.com>
2022-10-04 18:03:29 +0200

    added images and worked on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'valentin.de';
+export class AppComponent implements OnInit {
+  images = ['Grundloser See 3.jpg', 'Grundloser See 7.jpg', 'Grundloser See 8.jpg'];
+  currentImage = 0;
+  showImage = true;
 
-  sayhi() {
-    alert('hehe');
+  ngOnInit() {
+    this.updateImage();
   }
+
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 1);
+    }, 8000);
+  }
+
+
+
 }
